fd3b7caebf625a21bd6f4c143effd4ca7c3c4310
Aaron Blondeau <aaronblondeau@gmail.com>
2022-08-25 13:38:20 -0600

    Config for production (on stame box as staging).

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,13 @@ module.exports = {
     apps : [{
         name   : "mpg2co2",
         script: './index.js',
-        watch: '.'
+        watch: '.',
+        env_staging: {
+            "PORT": 3000
+        },
+        env_production: {
+            "PORT": 3001
+        }
     }],
   
     deploy : {
@@ -12,6 +18,15 @@ module.exports = {
             ref  : 'origin/staging',
             repo : 'git@github.com:aaronblondeau/mpg2co2.git',
             path : '/var/www/mpg2co2',
+            'post-setup': 'yarn install',
+            'post-deploy' : 'pm2 reload ecosystem.config.js --env staging',
+        },
+        staging : {
+            user : 'root',
+            host : '104.238.135.191',
+            ref  : 'origin/main',
+            repo : 'git@github.com:aaronblondeau/mpg2co2.git',
+            path : '/var/www/mpg2co2_production',
             'post-setup': 'yarn install',
             'post-deploy' : 'pm2 reload ecosystem.config.js --env staging',
         }
